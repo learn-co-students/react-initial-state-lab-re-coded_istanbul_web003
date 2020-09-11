fe86ea17ec8bfc333871595de082b41e7eaeda50
ibrahim aljarad <ibrahimealkalil@gmail.com>
2020-09-11 16:59:38 +0300

    Done.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,13 @@
-// your ImageSlider code here!
+import React from 'react';
+
+class ImageSlider extends React.Component {
+   render() {
+      return (
+         <div>
+            <Header/>
+            <Content/>
+         </div>
+      );
+   }
+}
+export default ImageSlider
